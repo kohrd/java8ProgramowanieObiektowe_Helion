048e4ae12a66e27592f69ee3f0b0c41a372ef43b
kohrd <konrad.janeczek@protonmail.com>
2019-01-17 15:09:24 +0100

    9.1

---
+++ src/main/java/pl/Java8_OOP_Helion/OOP_10_01/shapes/Shape.java
@@ -0,0 +1,18 @@
+package pl.Java8_OOP_Helion.OOP_10_01.shapes;
+
+
+public abstract class Shape {
+
+    public Shape() {
+        System.out.println("Jestem kontruktorem shape");
+
+    }
+
+    abstract int countPerimeter();
+
+    abstract int countArea();
+
+    public String getName() {
+        return getClass().getSimpleName();
+    }
+}
---
+++ src/main/java/pl/Java8_OOP_Helion/OOP_10_01/shapes/Square.java
@@ -0,0 +1,35 @@
+package pl.Java8_OOP_Helion.OOP_10_01.shapes;
+
+public final class Square extends Shape {
+
+    private int size;
+
+    public Square(){
+       super(); // przy wylowywaniu konstruktora square wywolany zostanie kontruktor shape
+        System.out.println("jestem kontruktorem square"); // ale to będzie uzyte tylko przy wywolaniu kontruktora bezargumentowego
+    }
+
+    public Square(int size){
+        this.size = size;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    void setSize(int size) {
+        this.size = size;
+    }
+
+    // napisanie metody
+    @Override
+    public int countArea(){
+        return size*size;
+    }
+
+    // napisanie metody
+    @Override
+    int countPerimeter(){
+        return 4*size;
+    }
+}
